a66d28bc148bcf42a33e0f536815c2831e1e4518
omarkami007 <114820791+omarkami007@users.noreply.github.com>
2024-03-06 16:22:55 +0100

    Update test_dash.py

--- test_dash.py
+++ test_dash.py
@@ -301,7 +301,11 @@ st.sidebar.text(f'End Date: {START_DATE}')
 
 
 btc_data = download_btc_data(ndays,time_step)
-news_df = download_news_data(ndays,time_step,API_KEY)
+try:
+    news_df = download_news_data(ndays,time_step,API_KEY)
+except Exception as e:
+    print(e)
+    print('News API Expired. Please type a new API key')
 sentiment = sentiment_analysis(news_df)
 overall = pd.merge(btc_data,sentiment,on='date',how='inner')
 
